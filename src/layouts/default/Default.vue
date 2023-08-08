0806116f17837795104884aaa742152abb71d200
AnweshDahal <anweshdahal2000@gmail.com>
2023-08-08 12:33:45 +0545

    setup: project structure

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,9 +1,43 @@
 <template>
   <v-app>
+    <header class="app-header">
+      <div class="app-logo">{{ appName }}</div>
+    </header>
     <default-view />
+    <footer class="app-footer">
+      <p class="copyright">
+        &copy;{{ new Date().getFullYear() }} {{ appName }}, All Rights Reserved
+      </p>
+    </footer>
   </v-app>
 </template>
 
-<script setup>
-  import DefaultView from './View.vue'
+<script>
+import DefaultView from "./View.vue";
+export default {
+  name: "Default",
+  components: {
+    "default-view": DefaultView,
+  },
+  data: () => ({
+    appName: import.meta.env.VITE_APP_NAME,
+  }),
+};
 </script>
+
+<style lang="scss" scoped>
+header.app-header {
+  padding: 1rem;
+  .app-logo {
+    font-size: 2rem;
+    font-family: "Manrope", sans-serif;
+    font-weight: 600;
+  }
+}
+
+.app-footer {
+  padding: 1rem;
+  text-align: center;
+  font-family: "Manrope", sans-serif;
+}
+</style>
---
+++ src/views/HomePage/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home-page">
+    <h1 class="sample-heading">This is the home page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style lang="scss" scoped>
+.home-page {
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+
+  .sample-heading {
+    font-family: "Manrope", sans-serif;
+  }
+}
+</style>
